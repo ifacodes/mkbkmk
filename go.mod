0a6a7fe2ea18749f2f30f0231e9ab59543fb5958
Aoife <aoifeebradley@gmail.com>
2020-12-28 14:16:18 -0500

    initial commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/poww10s/mkbkmk
+
+go 1.15
+
+require golang.org/x/net v0.0.0-20201224014010-6772e930b67b
---
+++ main.go
@@ -0,0 +1,42 @@
+package main
+
+import "os"
+import "net/http"
+
+import "golang.org/x/net/html"
+
+func panil(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
+
+func updateBookmarks(title string) {	
+	home, err := os.UserHomeDir()
+	panil(err)
+	
+	home+="/.bookmarks"
+	f, err := os.OpenFile(home, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	panil(err)
+	defer f.Close()
+
+	_, err = f.WriteString(os.Args[1] + "," + title + "\n")
+	panil(err)
+}
+
+func GetHtmlTitle(n *html.Node) {
+	if n.Type == html.ElementNode && n.Data == "title" {
+		updateBookmarks(n.FirstChild.Data)
+	}
+	for c := n.FirstChild; c != nil; c = c.NextSibling {
+		GetHtmlTitle(c)
+	}
+}
+
+func main() {
+	page, err := http.Get(os.Args[1])
+	panil(err)
+	doc, err := html.Parse(page.Body)
+	panil(err)
+	GetHtmlTitle(doc)
+}
